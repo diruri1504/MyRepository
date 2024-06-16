f2c13a74c474fa7b3ca1ed7a8485b4d973e1a236
diruri1504 <diruri1504>
2024-06-16 04:59:27 +0000

    Query ejemplo
    Query ejemplo

---
+++ Example/Querys.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC select * from catalog_lhcl_prod_bcp.bcp_edv_rbmpersdatadriver.archivo_condiciones_predicates_nuevo
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from catalog_lhcl_prod_bcp.bcp_edv_rbmpersdatadriver.hm_ventas_pp_003
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from catalog_lhcl_prod_bcp.bcp_edv_rbmpersdatadriver.hm_ventas_pp_003
